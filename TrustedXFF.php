cf858e6e2f0b6f6cdbe8936082d9d2e970ce904e
Justin Du <justin.d128@gmail.com>
2016-07-22 09:34:52 -0500

    Convert TrustedXFF to use extension registration
    Bug: T88065
    Change-Id: I387db114990c62d8eaeb709d8ee870ed79053f8f

--- TrustedXFF.php
+++ TrustedXFF.php
@@ -1,38 +1,14 @@
 <?php
-
-// Entry point protection
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'This is an extension to MediaWiki and cannot be run standalone.' );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'TrustedXFF' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['TrustedXFF'] = __DIR__ . '/i18n';
+	/*wfWarn(
+		'Deprecated PHP entry point used for TrustedXFF extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);*/
+	return;
+} else {
+	die( 'This version of the TrustedXFF extension requires MediaWiki 1.25+' );
 }
-
-/**
- * Trusted hosts file in CDB format.
- * The file can be generated using generate.php
- *
- * You can download Wikimedia's trusted-xff.cdb from:
- *
- * https://noc.wikimedia.org/conf/trusted-xff.cdb
- *
- * For details, see https://meta.wikimedia.org/wiki/XFF_project
- */
-$wgTrustedXffFile = $IP . '/cache/trusted-xff.cdb';
-
-// Register extension
-$wgExtensionCredits['other'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'TrustedXFF',
-	'descriptionmsg' => 'trustedxff-desc',
-	'author'         => 'Tim Starling',
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:TrustedXFF',
-	'license-name'   => 'GPL-2.0+'
-);
-
-// Load class
-$wgAutoloadClasses['TrustedXFF'] = __DIR__ . '/TrustedXFF.body.php';
-
-// I18n files
-$wgMessagesDirs['TrustedXFF'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['TrustedXFF'] = __DIR__ . '/TrustedXFF.i18n.php';
-
-// Load hook
-$wgHooks['IsTrustedProxy'][] = 'TrustedXFF::onIsTrustedProxy';
