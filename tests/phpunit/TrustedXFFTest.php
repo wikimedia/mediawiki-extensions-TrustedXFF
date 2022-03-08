086b1d97a9075bb9a4b2d8953aa0550bfd71900a
Reedy <reedy@wikimedia.org>
2022-03-08 00:35:01 +0000

    TrustedXFFTest: Swap to data provider and add more IP ranges
    Change-Id: I5e3f7e9e607a84d47a53e320cfeb7da2fe28c35c

--- tests/phpunit/TrustedXFFTest.php
+++ tests/phpunit/TrustedXFFTest.php
@@ -9,18 +9,27 @@ class TrustedXFFTest extends MediaWikiIntegrationTestCase {
 		TrustedXFF::$instance = null;
 	}
 
+	public function provideIPs() {
+		return [
+			[ '64.12.96.1', true, "IPv4 address trusted because it's in the AOL range 64.12.96.0/19" ],
+			[ '127.0.0.2', false, 'IPv4 address not trusted' ],
+			[ '2001:4c28:1::1', true, "IPv6 address trusted because it's in the Opera Mini range 2001:4c28:1::/48" ],
+			[ '2001:4c28:2::1', false, 'IPv6 address not trusted' ],
+		];
+	}
+
 	/**
+	 * @dataProvider provideIPs
+	 *
 	 * @covers MediaWiki\Extension\TrustedXFF\TrustedXFF::isTrusted
 	 * @covers MediaWiki\Extension\TrustedXFF\TrustedXFF::getInstance
 	 * @covers MediaWiki\Extension\TrustedXFF\TrustedXFF::onIsTrustedProxy
 	 */
-	public function testMissingXffFileIsHandledGracefully() {
-		$ip = '127.0.0.2';
+	public function testisTrusted( $ip, $expectedTrusted, $msg ) {
 		$trusted = false;
-
 		TrustedXFF::onIsTrustedProxy( $ip, $trusted );
 
-		$this->assertFalse( $trusted );
+		$this->assertEquals( $expectedTrusted, $trusted, $msg );
 	}
 
 }
