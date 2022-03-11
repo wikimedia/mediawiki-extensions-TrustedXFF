7c67b3dfb556f49b7ed87d7231865ed675777567
Reedy <reedy@wikimedia.org>
2022-03-11 21:27:06 +0000

    Add benchmark
    Bug: T303341
    Change-Id: I240af467a8f244241e01f8fd278ffe1bc0a01eb9

---
+++ benchmarks/BenchmarkLookup.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace MediaWiki\Extension\TrustedXFF;
+
+use Benchmarker;
+use const RUN_MAINTENANCE_IF_MAIN;
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/includes/Benchmarker.php";
+
+/**
+ * Benchmark the TrustedXFF Lookup
+ *
+ * @codeCoverageIgnore
+ */
+class BenchmarkLookup extends Benchmarker {
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription( 'Benchmark for TrustedXFF Lookup' );
+		$this->requireExtension( 'TrustedXFF' );
+	}
+
+	public function execute() {
+		$ips = [
+			// First IP in list - 400C6000
+			'64.12.96.0',
+			// IP from middle of list - 6BA773D7
+			'107.167.115.215',
+			// Last IP in list - D9ED97CD
+			'217.237.151.205',
+			// IP not in list
+			'127.0.0.1',
+		];
+
+		$xff = TrustedXFF::getInstance();
+
+		$benches = [];
+		foreach ( $ips as $ip ) {
+			$benches[] = [
+				'function' => [ $xff, 'isTrusted' ],
+				'args' => [ $ip ],
+			];
+		}
+		$this->bench( $benches );
+	}
+}
+
+$maintClass = BenchmarkLookup::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
